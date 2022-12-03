b10212eee89deeaeab5113725923acfea41f4596
qzhang21 <44728433+qzhang21@users.noreply.github.com>
2022-12-03 14:23:57 -0500

    Create hic.py template

---
+++ codes/hic.py
@@ -0,0 +1,17 @@
+# Hi-C class script
+
+def extract_data():
+    """
+    return a dataframe with genes as rows and samples as columns
+    make sure the genes are contained in the chromosome location info file (take intersection)
+    """
+    return None
+
+def get_genes_from_chromosome(chromo_num):
+    return None
+
+def slide_boundary():
+    return None
+
+def calc_tad_coexp():
+    return None
