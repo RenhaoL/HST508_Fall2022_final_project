d4e8a5a940bc48773d9be5bf23cca1061bd52636
qzhang21 <44728433+qzhang21@users.noreply.github.com>
2022-12-03 14:24:50 -0500

    hic.py packages

--- codes/hic.py
+++ codes/hic.py
@@ -1,10 +1,16 @@
 # Hi-C class script
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import zscore
 
 def extract_data():
     """
     return a dataframe with genes as rows and samples as columns
     make sure the genes are contained in the chromosome location info file (take intersection)
     """
+
     return None
 
 def get_genes_from_chromosome(chromo_num):
