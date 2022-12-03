efa19eebb267b5198930fdae731d84f8126e412c
qzhang21 <44728433+qzhang21@users.noreply.github.com>
2022-12-03 14:30:14 -0500

    hic.py extract_data

--- codes/hic.py
+++ codes/hic.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import zscore
 
-def extract_data():
+def extract_data(data_path="../data/single_cell_tpm.tsv",gene_loc_path="../data/gene_locations.tsv"):
     """
     return a dataframe with genes as rows and samples as columns
     make sure the genes are contained in the chromosome location info file (take intersection)
     """
-
-    return None
+    # read in data
+    sc_df = pd.read_csv(data_path,sep="\t",index_col=0)
+    # get rid of genes with 0 exp across all samples
+    sc_df_filtered = sc_df.loc[np.sum(sc_df,axis=1)!=0]
+    # read in gene location information
+    gloc = pd.read_csv(gene_loc_path,sep="\t",index_col=0)
+    return sc_df_filtered.loc[set(sc_df_filtered.index).intersection(gloc.index)]
 
 def get_genes_from_chromosome(chromo_num):
     return None
