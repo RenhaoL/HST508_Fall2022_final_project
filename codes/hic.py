175240cab0b8d1adb8a41189f6dce0cada608379
Renhao Luo <renhao_luo@hms.harvard.edu>
2022-12-07 15:30:09 -0500

    resolve conflict

--- codes/hic.py
+++ codes/hic.py
@@ -205,6 +205,24 @@ def calc_gene_dist(same_chrom_gene_pair,gene_loc):
     gene2_end = gene_loc.loc[gene2]['end']
     return abs((gene1_srt+gene1_end)/2-(gene2_srt+gene2_end)/2)
 
+def gene_dist_correlation(same_chrom_gene_pair, gene_corr_matrix_dir):
+
+    """Extract the correlation between two genes on the same chromosome from the chomosome specific correlation matrix
+
+    Returns:
+        float: correlation between the two given genes
+    """
+
+    assert len(same_chrom_gene_pair) == 2, "Only accept two genes as input"
+
+    gene_corr_matrix = pd.read_csv(gene_corr_matrix_dir)
+    gene1, gene2 = same_chrom_gene_pair[0], same_chrom_gene_pair[1]
+
+    assert str(gene1) in list(gene_corr_matrix.columns) and str(gene2) in list(gene_corr_matrix.columns), "The input gene pairs does not locate on the same chromosome"
+
+    return gene_corr_matrix.loc[gene1, gene2]
+    
+
 def genes_in_same_tad(gene_pair,tg_dict):
     """
     given a pair of genes as a tuple, determine whether the genes are in the same TAD.
@@ -258,7 +276,12 @@ def main():
         #         pairs_in_tad.append(pair)
         # print frequency of pairs in the same TAD
 
+<<<<<<< HEAD
         # analysis 3: correlation as a function of distance between genes
+=======
+    # analysis 3: correlation as a function of distance between genes
+    
+>>>>>>> b3414c4 (add func to calculate gene dist correlation)
 
 if __name__ == "__main__":
     main()
---
+++ codes/upsetplot_overlap_genes.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+
+from matplotlib import pyplot as plt
+from upsetplot import generate_counts, plot
+
+
+example = generate_counts()
+print(example)
